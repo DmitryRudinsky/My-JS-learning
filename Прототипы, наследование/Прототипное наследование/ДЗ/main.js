aa5d144aabdaa2fde9e02e2cecb7c16b3975455e
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-11-29 01:02:27 +0300

    Prototypes, inheritance

---
+++ Прототипы, наследование/Прототипное наследование/ДЗ/main.js
@@ -0,0 +1,21 @@
+// 1 задание
+
+let head = {
+    glasses: 1
+};
+let table = {
+    pen: 3,
+    __proto__: head,
+};
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table,
+};
+let pockets = {
+    money: 2000,
+    __proto__: bed,
+};    
+
+alert(pockets.pen); //3
+alert(bed.glasses); //1
