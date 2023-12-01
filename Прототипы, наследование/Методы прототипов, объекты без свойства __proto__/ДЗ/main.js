3a395af4375663b9bdefaf9624a52df9b2874acb
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-12-01 12:25:45 +0300

    Методы прототипов.ДЗ

---
+++ Прототипы, наследование/Методы прототипов, объекты без свойства __proto__/ДЗ/main.js
@@ -0,0 +1,18 @@
+// 1 задание
+
+let dictionary = Object.create(null, {
+    toString: {
+        value(){
+            return Object.keys(this).join();
+        }
+    }
+});
+
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test";
+
+for(let key in dictionary) {
+    alert(key); // "apple", затем "__proto__"
+}
+alert(dictionary);
