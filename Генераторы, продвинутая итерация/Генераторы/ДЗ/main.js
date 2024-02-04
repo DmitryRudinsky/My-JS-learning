c88c8110bb4be003817ae270e09aa51e625a2166
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-02-04 20:51:24 +0300

    Генераторы

---
+++ Генераторы, продвинутая итерация/Генераторы/ДЗ/main.js
@@ -0,0 +1,16 @@
+//1 задание
+
+function* pseudoRandom(seed){
+    let value = seed;
+    while(true){
+        value = value * 16807 % 2147483647
+        yield value;
+    }
+}
+
+let generator = pseudoRandom(1);
+
+console.log(generator.next().value); //16807
+console.log(generator.next().value); //282475249
+console.log(generator.next().value); //1622650073
+console.log(generator.next().value); //984943658
