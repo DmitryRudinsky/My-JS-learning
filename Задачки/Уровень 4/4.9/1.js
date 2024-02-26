9e43c68e02847c8ae2676501c89a78f5e195fd0b
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-02-27 01:25:24 +0300

    4.9

---
+++ Задачки/Уровень 4/4.9/1.js
@@ -0,0 +1,12 @@
+"Сделайте функцию, которая будет возвращать случайный цвет."
+
+function randomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+}
+
+randomColor();
---
+++ Задачки/Уровень 4/4.9/2.js
@@ -0,0 +1,39 @@
+"Сделайте функцию, которая параметром будет принимать массив чисел и возвращать массив общих делителей всех чисел из переданного массива."
+
+function arrayOfDivs(n){
+    let arrOfDivs = [];
+    for(let i = 1; i * i < n; i++){
+        if (n % i == 0){
+            arrOfDivs.push(i);
+            if(n / i != i) arrOfDivs.push(n / i);
+        }
+    }
+    return arrOfDivs.sort((a, b) => a - b);
+}
+
+function count(array, n){
+    let cnt = 0;
+    for(let elem of array){
+        if(elem == n) cnt += 1;
+    }
+    return cnt;
+}
+
+function arrOfMutualDivs(array){
+    let newArr = [];
+    for(let elem of array){
+        let divsOfElem = arrayOfDivs(elem);
+        divsOfElem.forEach(element => {
+            newArr.push(element);
+        });
+    }
+    let divArr = new Set();
+    for(let i = 0; i < newArr.length; i++){
+        if (count(newArr, newArr[i]) == array.length){
+            divArr.add(newArr[i]);
+        }
+    }
+    return [...divArr];
+}
+
+console.log(arrOfMutualDivs([720, 5040, 360, 234])); //[ 1, 2, 3, 6, 9, 18 ]
